d2c38bec402661985497b5b2e21b4100463d0b11
st10676 <st10676@iismarconipieralisi.it>
2025-03-07 10:17:29 +0100

    prima versione

--- app/src/main/java/com/example/miaprimaapplicazione/RegisterActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.miaprimaapplicazione;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //collegamento con elementi grafici
+        EditText nome = findViewById(R.id.NomeInserimento);
+        EditText cognome = findViewById(R.id.CognomeInserimento);
+        EditText dataNascita = findViewById(R.id.DataNascitaInserimento);
+        EditText username = findViewById(R.id.UsernameInserimento);
+        EditText password = findViewById(R.id.PasswordInserimento);
+        EditText email = findViewById(R.id.EmailInserimento);
     }
 }
